5f3d0217703f819fe11297f8355ea00c2b13c7df
Craig <craig@foraefactory.com>
2025-05-23 09:46:42 -0500

    Styles added to match wireframe

--- finalcap/src/Components/Nav.js
+++ finalcap/src/Components/Nav.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Logo from '../Images/Logo.svg';
+import '../Nav.css';
 
 
 function Nav() {
   return (
-    <nav>
-        <img src={Logo} alt="Little Lemon Logo"></img>
+    <nav className='nav'>
+        <img id="ll-logo" src={Logo} alt="Little Lemon Logo"></img>
         <ul>
           <li><a href='#/Home'>Home</a></li>
           <li><a href='#/About'>About</a></li>
